28ee17d1e962f99f535824ddc1015d0295f7957e
Jon Rimmer <jon.rimmer@gmail.com>
2019-07-06 12:42:28 +0100

    Render to SVG

---
+++ build/render.js
@@ -0,0 +1,18 @@
+export const pointsToSvg = (points) => {
+    return points
+        .map(({ x, y }, i) => `
+    <circle class="${i === 0 ? 'start' : 'point'}" cx="${x}" cy="${y}" r="${i === 0 ? 5 : 3}" />
+  `)
+        .join('');
+};
+export const routeToSvg = (points, route) => {
+    return route
+        .map((p, i) => {
+        const { x: x1, y: y1 } = points[p];
+        const { x: x2, y: y2 } = points[i === route.length - 1 ? route[0] : route[i + 1]];
+        return `
+      <line class="link" x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" />
+    `;
+    })
+        .join('');
+};
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
 import { getRoute } from './nearest-neighbor.js';
 import { calcCost, Point } from './route.js';
+import { pointsToSvg, routeToSvg } from './render.js';
 
-const pointsCanvas = document.getElementById('points') as HTMLCanvasElement;
-const routeCanvas = document.getElementById('route') as HTMLCanvasElement;
+//TS types are wrong: https://github.com/microsoft/TypeScript/issues/4689
+const pointsSvg = (document.getElementById(
+  'points'
+) as unknown) as SVGSVGElement;
+const routeSvg = (document.getElementById('route') as unknown) as SVGGElement;
+
 const bestCost = document.getElementById('bestCost') as HTMLSpanElement;
-
-const ptsCtx = pointsCanvas.getContext('2d')!;
-const rtCtx = routeCanvas.getContext('2d')!;
-
-let width = 0;
-let height = 0;
-
-let scaleX: (v: number) => number;
-let scaleY: (v: number) => number;
 
 let points: Point[];
 
@@ -24,33 +20,33 @@ const LINE_STYLE = 'green';
 
 const setPoints = (pts: Point[]) => {
   points = pts;
-  let maxX = 0;
-  let maxY = 0;
+  let minX = Number.POSITIVE_INFINITY;
+  let minY = Number.POSITIVE_INFINITY;
+  let maxX = Number.NEGATIVE_INFINITY;
+  let maxY = Number.NEGATIVE_INFINITY;
 
   points.forEach(({ x, y }) => {
     maxX = Math.max(maxX, x);
     maxY = Math.max(maxY, y);
+    minX = Math.min(minX, x);
+    minY = Math.min(minY, y);
   });
 
-  let padX = maxX * 0.02;
-  let padY = maxY * 0.02;
+  const pad = 0.01;
+  const width = maxX - minX;
+  const height = maxX - minY;
 
-  maxX += padX * 2;
-  maxY += padY * 2;
+  minX -= width * pad;
+  minY -= height * pad;
+  maxX += 2 * (width * pad);
+  maxY += 2 * (height * pad);
 
-  scaleX = x => (x / maxX) * width;
-  scaleY = y => (y / maxY) * height;
+  routeSvg.setAttribute('viewBox', `${minX} ${minY} ${maxX} ${maxY}`);
+  pointsSvg.setAttribute('viewBox', `${minX} ${minY} ${maxX} ${maxY}`);
 };
 
 const renderPoints = () => {
-  ptsCtx.clearRect(0, 0, width, height);
-
-  points.forEach(({ x, y }, i) => {
-    ptsCtx.fillStyle = i == 0 ? HOME_STYLE : POINT_STYLE;
-    ptsCtx.beginPath();
-    ptsCtx.arc(scaleX(x), scaleY(y), POINT_RADIUS, 0, 2 * Math.PI, false);
-    ptsCtx.fill();
-  });
+  pointsSvg.innerHTML = pointsToSvg(points);
 };
 
 const renderRoute = () => {
@@ -58,25 +54,7 @@ const renderRoute = () => {
 
   bestCost.textContent = `${calcCost(points, route)}`;
 
-  rtCtx.clearRect(0, 0, width, height);
-
-  rtCtx.strokeStyle = LINE_STYLE;
-
-  // Home
-  const { x: ox, y: oy } = points[0];
-
-  // Start at home
-  rtCtx.moveTo(scaleX(ox), scaleY(oy));
-  rtCtx.beginPath();
-
-  route.forEach(i => {
-    const { x, y } = points[i];
-    rtCtx.lineTo(scaleX(x), scaleY(y));
-  });
-
-  // Return home
-  rtCtx.lineTo(scaleX(ox), scaleY(oy));
-  rtCtx.stroke();
+  routeSvg.innerHTML = routeToSvg(points, route);
 };
 
 const render = () => {
@@ -84,18 +62,7 @@ const render = () => {
   renderRoute();
 };
 
-const resize = () => {
-  const rect = pointsCanvas.getBoundingClientRect();
-  width = rect.width;
-  height = rect.height;
-  routeCanvas.width = pointsCanvas.width = width;
-  routeCanvas.height = pointsCanvas.height = height;
-  render();
-};
-
-const pointsData = document.getElementById('points400') as HTMLScriptElement;
+const pointsData = document.getElementById('points200') as HTMLScriptElement;
 setPoints(JSON.parse((pointsData.textContent || '[]').trim()));
 
-requestAnimationFrame(resize);
-
-window.addEventListener('resize', resize);
+requestAnimationFrame(render);
